2563bcecc6a8bf92ad39185a5bea040471d2ac79
eshk3re <105351013+eshk3re@users.noreply.github.com>
2023-04-16 17:41:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def calculate():
     return jsonify({'result': result})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
